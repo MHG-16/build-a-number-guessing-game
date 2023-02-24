7a9465d21c4f129548b464abf739e907663a8fc0
MHG-16 <hammahadi2006@gmail.com>
2023-02-24 07:00:20 +0000

    feat: main FUNCTION

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo -e "\n~~ Number guessing game ~~\n"
+MAIN(){
+  echo -e "\nEnter your username:"
+  read USER_NAME
+  SEARCH_USER $USER_NAME
+}
+
+SEARCH_USER(){
+  echo $1
+}
+
+MAIN
